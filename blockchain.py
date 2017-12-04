0d1c79d0dae7ce258b8418efc32ac6208e14088f
prtx <prtx.pratikshrestha@gmail.com>
2017-12-04 21:38:30 +0545

    Add basic chain implementation.

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,17 @@
 class Block:
 
-    def __init__(self, previous_hash, transactions):
+    def __init__(self, previous_hash=None, transactions=()):
         self.previous_hash = previous_hash
         self.transactions = transactions
-        self.this_hash = hash(transactions + (previous_hash,))
+        self.this_hash = hash((previous_hash,) + transactions)
+
+    
+class Chain:
+    
+    def __init__(self):
+        genesis_block = Block()
+        self.chain = [genesis_block]
+
+    def register_block(self, transactions):
+        self.chain.append(Block(self.chain[-1].this_hash, transactions))
 
