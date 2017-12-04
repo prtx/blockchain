6884c49a90474ee588207d3763420372772d069d
prtx <prtx.pratikshrestha@gmail.com>
2017-12-04 22:04:27 +0545

    Add display.

--- blockchain.py
+++ blockchain.py
@@ -12,12 +12,30 @@ class Block:
         self.timestamp = datetime.datetime.now()
 
     
+    def __repr__(self):
+        return "transactions: %s, timestamp: %s" % (str(self.transactions), str(self.timestamp))
+
+
 class Chain:
     
     def __init__(self):
         genesis_block = Block()
         self.chain = [genesis_block]
 
+
     def register_block(self, transactions):
         self.chain.append(Block(self.chain[-1].this_hash, transactions))
+    
+    
+    def __repr__(self):
+        out = ""
+        for block in self.chain:
+            out += "%s\n" % str(block)
+        return out
 
+
+if __name__ == "__main__":
+    chain = Chain()
+    chain.register_block((1, 2, 3,))
+    chain.register_block((1, 2, 3, 4,))
+    print(chain)
