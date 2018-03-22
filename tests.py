bb95000cf93430569410789f9047967dac0b03ca
prtx <prtx.pratikshrestha@gmail.com>
2018-03-22 12:12:32 +0545

    Segregate tests for blocks and chains

--- tests.py
+++ tests.py
@@ -1,23 +1,39 @@
 import unittest
 import datetime
 
-import blockchain
+from blockchain import Block, Chain
 
 
-class BlockchainTests(unittest.TestCase):
+class BlockTests(unittest.TestCase):
+    """A block records transactions in a blockchain. Thus a block is like a
+    page of a ledger or record book. Each time a block is 'completed', it
+    gives way to the next block in the blockchain."""
 
     def test_genesis_block(self):
-        genesis_block = blockchain.Block()
+        """A genesis block is the first block of a block chain. Modern
+        versions of Bitcoin number it as block 0, though very early versions
+        counted it as block 1. The genesis block is almost always hardcoded
+        into the software of the applications that utilize its block chain."""
+
+        # An empty block is a genesis block.
+        genesis_block = Block()
         self.assertTrue(genesis_block)
+
+        # Following are attributes of a block and values for genesis block.
         self.assertEqual(genesis_block.previous_hash, None)
         self.assertEqual(genesis_block.transactions, [])
         self.assertEqual(type(genesis_block.timestamp), datetime.datetime)
+        self.assertEqual(type(genesis_block.this_hash), int) # more on this hash calculation later
 
 
     def test_transaction_block(self):
-        genesis_block = blockchain.Block()
+        """Every new block uses the hash of the previous one and transactions
+        within the block. It is meant to be completely unique and dependent on
+        data from previous block.
+            this_hash = hash(prev_block_hash, timestamp, transactions))"""
+        genesis_block = Block()
         transactions = [1, 2, 3]
-        transaction_block = blockchain.Block(genesis_block.this_hash, transactions)
+        transaction_block = Block(previous_hash=genesis_block.this_hash, transactions=transactions)
 
         self.assertTrue(transaction_block)
         self.assertEqual(transaction_block.previous_hash, genesis_block.this_hash)
@@ -26,22 +42,55 @@ class BlockchainTests(unittest.TestCase):
         self.assertTrue(transaction_block.timestamp > genesis_block.timestamp)
 
 
+class BlockChainTests(unittest.TestCase):
+    """A blockchain is a continuously growing list of blocks linked and secured
+    using cryptography. By design, a blockchain is inherently immutable. It is
+    "an open, distributed ledger that can record transactions between two
+    parties efficiently and in a verifiable and permanent way"."""
     def test_chain(self):
-        chain = blockchain.Chain()
-        self.assertTrue(chain)
+        blockchain = Chain()
+        self.assertTrue(blockchain)
+
+        # First block in the chain is always a genesis block.
+        self.assertEqual(len(blockchain), 1)
+        self.assertEqual(type(blockchain[0]), Block)
+        self.assertEqual(blockchain[0].previous_hash, None)
+        self.assertEqual(blockchain[0].transactions, [])
+
+
+    def test_add_block(self):
+        blockchain = Chain()
+
+        # A block is appended in the end of the list(chain).
+        transactions1 = [1, 2, 3,]
+        blockchain.register_block(transactions=transactions1)
+        self.assertEqual(len(blockchain), 2)
+        self.assertEqual(blockchain[-1].transactions, transactions1)
+
+        transactions2 = [1, 2, 3, 4,]
+        blockchain.register_block(transactions=transactions2)
+        self.assertEqual(len(blockchain), 3)
+        self.assertEqual(blockchain[-1].transactions, transactions2)
+
+        # The hashes are maintained along the chain.
+        self.assertEqual(blockchain[0].this_hash, blockchain[1].previous_hash)
+        self.assertEqual(blockchain[1].this_hash, blockchain[2].previous_hash)
 
 
     def test_chain_validation(self):
+        """A blockchain ledger should be immutable. To make sure we validate it
+        by traversing through the chain to make sure previous hash of a block
+        is indeed the hash of previous block."""
+        blockchain = Chain()
         transactions1 = [1, 2, 3,]
+        blockchain.register_block(transactions=transactions1)
         transactions2 = [1, 2, 3, 4,]
+        blockchain.register_block(transactions=transactions2)
+        self.assertTrue(blockchain.isvalid())
 
-        chain = blockchain.Chain()
-        chain.register_block(transactions1)
-        chain.register_block(transactions2)
-        self.assertTrue(chain.isvalid())
-
-        chain.chain[1].transactions[1] = 11
-        self.assertFalse(chain.isvalid())
+        # In case of any mutation the hash condition of the whole chain is invalid.
+        blockchain[1].transactions[1] = 11
+        self.assertFalse(blockchain.isvalid())
 
 
 unittest.main()
